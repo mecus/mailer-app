a0834c125813925ab1272ac4b01f45558ea2c304
MECUS <design@miscotech.co.uk>
2017-04-10 13:02:49 +0100

    created_at update

--- app/resources/order_resource.rb
+++ app/resources/order_resource.rb
@@ -1,6 +1,6 @@
 class OrderResource < JSONAPI::Resource
   has_many :coffees, dependent: :destroy
   attributes :name, :email, :telephone, :address, :postcode, :city, :country,
-  :amount, :orderid, :status, :itemsid
+  :amount, :orderid, :status, :itemsid, :created_at
   # filter :status, default: 'pending'
 end
